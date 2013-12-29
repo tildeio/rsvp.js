c3f0dbf5e9d5c40b604a74ce5ecdb0c848c6505b
Stefan Penner <stefan.penner@gmail.com>
2013-12-29 02:03:56 -0500

    fixup browser globals build

--- tasks/browser.js
+++ tasks/browser.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
   grunt.registerMultiTask('browser', 'Export the object in <%= pkg.name %> to the window', function() {
     this.files.forEach(function(f) {
-      var output = ['(function(globals) {'];
+      var output = ['(function(global) {'];
 
       output.push.apply(output, f.src.map(grunt.file.read));
 
-      output.push("window.<%= pkg.namespace %> = requireModule('<%= pkg.name %>');");
+      output.push("global.<%= pkg.namespace %> = requireModule('<%= pkg.name %>');");
       output.push('}(window));');
 
       grunt.file.write(f.dest, grunt.template.process(output.join('\n')));
