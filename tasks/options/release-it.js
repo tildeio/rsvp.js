1a6242257edb7ca6693908dda9de9421c372720f
Stefan Penner <stefan.penner@gmail.com>
2013-12-30 22:09:49 -0500

    upgrade release repo's

--- tasks/options/release-it.js
+++ tasks/options/release-it.js
@@ -1,7 +1,8 @@
 module.exports = {
   options: {
     pkgFiles: [ 'package.json', 'bower.json' ],
-    distRepo: 'git@github.com:components/rsvp.js.git',
+    srcRepo: 'origin',
+    distRepo: 'git@github.com:components/rsvp.git',
     distStageDir: 'tmp/stage',
     distFiles: [ 'dist/rsvp.js', 'dist/rsvp.min.js', 'dist/rsvp.amd.js' ],
     distBase: 'dist'
