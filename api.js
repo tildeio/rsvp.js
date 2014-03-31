c65a9f8990b585e5c10f1ca8a849a20430967ffb
Mike Munroe <mike@mikemunroe.com>
2014-03-30 22:03:34 -0400

    first iteration on adding an API docs site
    for now, the site is manually generated using grunt docs task and uses
    default YUIdoc styles
    
    future iteration should be similar to jekyll site used for EAK - see
    https://github.com/stefanpenner/ember-app-kit/tree/gh-pages
    
    future iterations should also include styles that match other Ember
    sites

---
+++ api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "RSVP",
+        "RSVP.EventTarget",
+        "RSVP.Promise"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
