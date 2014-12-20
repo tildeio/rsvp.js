1851dfe23c21daa9f796364f6d8dd1011e45a7ec
Stefan Penner <stefan.penner@gmail.com>
2014-12-19 21:55:31 -0500

    fixup stuff

--- Brocfile.js
+++ Brocfile.js
@@ -57,6 +57,7 @@ function createTest(tree, testSupportPaths) {
       var pieces = path.split('/');
       var path = pieces.splice(0, pieces.length - 1).join('/');
       var file = pieces.splice(-1);
+
       return new Funnel(path, {
         include: [new RegExp( file )],
         exclude: [new RegExp('lib')],
@@ -65,7 +66,7 @@ function createTest(tree, testSupportPaths) {
     })
   );
 
-  var tests = watchify(tree,{
+  var tests = watchify(tree, {
     browserify: {
       entries: ['./main.js'],
       debug: true
@@ -74,7 +75,6 @@ function createTest(tree, testSupportPaths) {
     cache: true,
     init: function (b) {
       b.external('vertx');
-      b.external('../dist/rsvp.js');
     }
   });
 
